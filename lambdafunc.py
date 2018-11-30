e55f9470660fd0a605cd7e0c828b6a96ee5ba309
ajixnet <stuff@ajix.net>
2018-11-30 17:58:17 -0500

    Update lambdafunc.py

--- lambdafunc.py
+++ lambdafunc.py
@@ -6,11 +6,6 @@ import uuid
 import csv
      
 s3_client = boto3.client('s3')
-     
-def resize_image(image_path, resized_path):
-    with Image.open(image_path) as image:
-        image.thumbnail(tuple(x / 2 for x in image.size))
-        image.save(resized_path)
 
 def csv2json(s3obj):
 	csvfile = open(s3obj,'r')
@@ -23,12 +18,12 @@ def csv2json(s3obj):
 		jsonfile.write('\n')
      
 def handler(event, context):
-    for record in event['Records']:
-        bucket = record['s3']['bucket']['name']
-        key = record['s3']['object']['key'] 
-        download_path = '/tmp/{}{}'.format(uuid.uuid4(), key)
-        upload_path = '/tmp/resized-{}'.format(key)
-        
-        s3_client.download_file(bucket, key, download_path)
-        #resize_image(download_path, upload_path)
-        s3_client.upload_file(upload_path, '{}resized'.format(bucket), key)
+	for record in event['Records']:
+		bucket = record['s3']['bucket']['name']
+		key = record['s3']['object']['key'] 
+		download_path = '/tmp/{}{}'.format(uuid.uuid4(), key)
+		upload_path = '/tmp/resized-{}'.format(key)
+	
+	s3_client.download_file(bucket, key, download_path)
+	s3_client.upload_file(upload_path, '{}resized'.format(bucket), key)
+	
