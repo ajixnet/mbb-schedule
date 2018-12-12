76e1db9b041a684234576363fbdf9d57c446221d
ajixnet <stuff@ajix.net>
2018-12-11 21:09:15 -0500

    Update lambdafunc.py

--- lambdafunc.py
+++ lambdafunc.py
@@ -12,13 +12,13 @@ def csv2json(s3obj):
 	csvfile = open(s3obj,'r')
 	jsonfile = open('/tmp/mbb.json','a')
 	
-	fieldnames = ("opponent","isHome","date","day","time")
+	fieldnames = ("opponent","isHome","month","day","dow","time","scheduled")
 	reader = csv.DictReader(csvfile,fieldnames)
 	
 	print("inside the csv2json function")
 	#print(csvfile.read())
 	#print(csvfile,jsonfile,fieldnames,reader)
-	print("going into loop")
+	#print("going into loop")
 	for row in reader:
 		jsonfile.write(json.dumps(row))
 		jsonfile.write('\n')
@@ -28,7 +28,7 @@ def csv2json(s3obj):
     
 def upjson2dynamo(filepath, tablename):
 	print("inside the upload function")
-	#tablename = "MBB-Schedule"
+	#tablename = "MBB-Schedule-date"
 	jsonfile = open(filepath,'r')
 		
 	dynamodb = boto3.resource('dynamodb')
@@ -55,13 +55,14 @@ def lambda_handler(event, context):
 	s3_client.download_file(bucket, key, download_path)
 	upload_path, jsonfile = csv2json(download_path)
 	#s3_client.upload_file('/tmp/mbb.json', bucket, 'mbb.json')
-	print('Function finished - 1')
+	#print('Function finished - 1')
 	#jsonfile2 = open('/tmp/mbb.json', 'r')
 	#with open('/tmp/mbb.json', 'r') as myfile:
 	#	data=myfile.read()
 	#print(data)
 	#print("After read")
 	#print(upload_path,jsonfile,jsonfile2)
-	upjson2dynamo(upload_path,"MBB-Schedule")	
-	print("Function finished - 2")
+	upjson2dynamo(upload_path,"MBB-Schedule-date")	
+	#print("Function finished - 2")
+	return
 	
